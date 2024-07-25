5da2162a8669fd71cf3b75238ce8175faaf51cc1
Vanessa <vanessa.vasconcelos434@gmail.com>
2024-07-25 11:56:28 -0300

    Finalização de Ambiente Virtual

---
+++ Aula/Seção_4_Intermediario/pyhton_context_manager_with.py
@@ -0,0 +1,14 @@
+'''
+Criando arquivos com Python
+Usamos a função open para abrir um arquivo em Python (ele pode ou não existir)
+Modos:
+  r (leitura), w (escrita), x (para criação)
+'''
+
+
+
+
+
+caminho_arquivo =  'Aula\\Seção_4_Intermediario\\Teste_Aula_Python_Context\\'
+caminho_arquivo += 'arquivo_teste_caminho.py'
+print(caminho_arquivo)
